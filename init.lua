89791fff04d2772f4f50cf02889d9fb492704c10
Nicol Zabatta <nicolazabatta@gmail.com>
2022-06-18 14:47:09 +0800

    added code for powershelll as default termianl

--- init.lua
+++ init.lua
@@ -125,3 +125,14 @@ require('lualine').setup {
   tabline = {},
   extensions = {}
 }
+
+--------------------------
+--pwsh as default terminal
+--------------------------
+vim.api.nvim_exec([[
+let &shell = has('win32') ? 'powershell' : 'pwsh'
+let &shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
+let &shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+set shellquote= shellxquote=
+]], false)
